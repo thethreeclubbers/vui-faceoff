f3ee2d6c454db95491af8df4eeff1c8286455529
Baruch Sadogursky <jbaruch@jfrog.com>
2017-10-04 16:48:34 -0700

    rest api canceled (java8)

--- google/fulfillment/src/main/webapp/WEB-INF/groovy/hello.groovy
+++ google/fulfillment/src/main/webapp/WEB-INF/groovy/hello.groovy
@@ -1,6 +1,6 @@
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
-import static groovyx.net.http.HttpBuilder.configure
+//import static groovyx.net.http.HttpBuilder.configure
 
 import java.util.logging.Logger
 
@@ -10,11 +10,11 @@ def str = request.reader.text
 
 Logger.getLogger('hello').severe(str as String)
 
-def http = configure {
-    request.uri = 'https://vui-faceoff.atlassian.net'
-    request.auth.basic 'jbaruch@sadogursky.com', 'olUfgt3ReDXthhZvz58j85CD'
-    request.headers['Authorization'] = "Basic ${"jbaruch@sadogursky.com:olUfgt3ReDXthhZvz58j85CD".bytes.encodeBase64().toString()}" as String
-}
+//def http = configure {
+//    request.uri = 'https://vui-faceoff.atlassian.net'
+//    request.auth.basic 'jbaruch@sadogursky.com', 'olUfgt3ReDXthhZvz58j85CD'
+//    request.headers['Authorization'] = "Basic ${"jbaruch@sadogursky.com:olUfgt3ReDXthhZvz58j85CD".bytes.encodeBase64().toString()}" as String
+//}
 
 def text = slurper.parseText(str)
 def action = text.result.action
@@ -25,10 +25,11 @@ def defect
 def defects = ['WTF-18', 'WTF-28', 'WTF-78']
 switch (action) {
     case 'STANDUP_STATUS':
-        result = http.get {
-            request.uri.path = '/rest/api/2/search'
-            request.uri.query = [jql: 'status in ("In Progress", "To Do", "In Review")']
-        }
+//        result = http.get {
+//            request.uri.path = '/rest/api/2/search'
+//            request.uri.query = [jql: 'status in ("In Progress", "To Do", "In Review")']
+//        }
+        def result = [total:24]
         responseText = "There are ${result.total} open critical issues."
         break
     case 'TOP_USER_DEFECTS':
@@ -38,11 +39,11 @@ switch (action) {
         break
     case 'MORE_USER_DEFECTS':
         def name = text.result.parameters.username
-        responseText = "There are 100 more defects for $name"
+        responseText = "There are 100 more defects for $name! What a shame!"
         break
     case 'DEFECT_SEVERITY':
         defect = text.result.parameters.defect_id
-        responseText = "Defect $defect is a catastrophic one!"
+        responseText = "Oh, no! Defect $defect is a catastrophic one! Someone needs to do something!"
         ctxOut << [name: 'current-defect-id', parameters: [result_defect_id: defect]]
         break
     case 'BLOCK_DEFECT':
