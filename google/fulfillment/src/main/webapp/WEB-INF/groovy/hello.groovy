a021248e4184d52db5deea4247b2f43c64f8848b
Baruch Sadogursky <jbaruch@jfrog.com>
2017-11-09 12:02:17 +0100

    conversations, null handling, context propagation, random defects, no input prompt

--- google/fulfillment/src/main/webapp/WEB-INF/groovy/hello.groovy
+++ google/fulfillment/src/main/webapp/WEB-INF/groovy/hello.groovy
@@ -1,63 +1,65 @@
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
-//import static groovyx.net.http.HttpBuilder.configure
 
-import java.util.logging.Logger
-
+//region setup
+def random = new Random()
 def slurper = new JsonSlurper()
-
 def str = request.reader.text
-
-Logger.getLogger('hello').severe(str as String)
-
-//def http = configure {
-//    request.uri = 'https://vui-faceoff.atlassian.net'
-//    request.auth.basic 'jbaruch@sadogursky.com', 'olUfgt3ReDXthhZvz58j85CD'
-//    request.headers['Authorization'] = "Basic ${"jbaruch@sadogursky.com:olUfgt3ReDXthhZvz58j85CD".bytes.encodeBase64().toString()}" as String
-//}
-
 def text = slurper.parseText(str)
 def action = text.result.action
-
-def responseText = 'I didn\'t get this one'
 def ctxOut = []
 def defect
-def defects = ['WTF-18', 'WTF-28', 'WTF-78']
+def defects = []
+3.times {
+    defects << "WTF-${random.nextInt(100)}"
+}
+def responseText = 'I didn\'t get this one'
+def reprompt = [{ speech 'Hey, are you there?' }]
+//endregion
+
 switch (action) {
+    //region STANDUP_STATUS
     case 'STANDUP_STATUS':
 //        result = http.get {
 //            request.uri.path = '/rest/api/2/search'
 //            request.uri.query = [jql: 'status in ("In Progress", "To Do", "In Review")']
 //        }
-        def result = [total:24]
+        def result = [total:random.nextInt(100)]
         responseText = "There are ${result.total} open critical issues."
         break
+    //endregion
+    //region TOP_USER_DEFECTS
     case 'TOP_USER_DEFECTS':
         def name = text.result.parameters.username
         responseText = "The top 3 defects for $name are: ${defects.join(",")}"
         ctxOut << [name: 'current-user-id', parameters: [username: name]]
         break
+    //endregion
+    //region MORE_USER_DEFECTS
     case 'MORE_USER_DEFECTS':
         def name = text.result.parameters.username
-        responseText = "There are 100 more defects for $name! What a shame!"
+        responseText = "There are ${random.nextInt(100)} more defects for $name! Not your best day, $name, is it?"
         break
+    //endregion
+    //region DEFECT_SEVERITY
     case 'DEFECT_SEVERITY':
         defect = text.result.parameters.defect_id
         responseText = "Oh, no! Defect $defect is a catastrophic one! Someone needs to do something!"
-        ctxOut << [name: 'current-defect-id', parameters: [result_defect_id: defect]]
         break
+    //endregion
+    //region BLOCK_DEFECT
     case 'BLOCK_DEFECT':
         defect = text.result.parameters.defect_id
         responseText = "Defect $defect is a blocker now, go fix it!"
         break
+    //endregion
 }
 
 def json = new JsonBuilder()
-
 json {
     speech responseText
     displayText responseText
+    noInputPrompts reprompt
     if (ctxOut) contextOut ctxOut
 }
-
 println json
